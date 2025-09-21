26489f466d368dc1d85ace98ea247a2835428415
MDSanju <mdsanju387@gmail.com>
2025-09-21 15:52:44 +0600

    fix ternary operatop

--- src/question-mark.ts
+++ src/question-mark.ts
@@ -5,35 +5,33 @@ const age: number = 27;
 const isAdult = age >= 18 ? "Yes" : "No";
 console.log(isAdult);
 
-
 // Nullish coalescing operator
 
 const isAuthUser = null; // It will work only for 'null' & 'undefined' value
-const userName = isAuthUser ?? 'Guest';
+const userName = isAuthUser ?? "Guest";
 
 console.log(userName);
 
-
-// 
+//
 
 type UserType = {
-    name: string;
-    age: number;
-    address: {
-        city: string;
-        road: string;
-        houseNo?: number;
-    }
-}
+  name: string;
+  age: number;
+  address: {
+    city: string;
+    road: string;
+    houseNo?: number;
+  };
+};
 
 const user007: UserType = {
-    name: 'John',
-    age: 35,
-    address: {
-        city: 'California',
-        road: 'Frontage Rd',
-    }
-}
+  name: "John",
+  age: 35,
+  address: {
+    city: "California",
+    road: "Frontage Rd",
+  },
+};
 
-const home = user007.address.houseNo ?? 'NA'; // Default value set
+const home = user007?.address?.houseNo ?? "NA"; // Default value set
 console.log({ home });
